badf508196531ccac231fdb1577af5807a9c0605
Aric Repp <aricmrepp@gmail.com>
2021-01-11 19:56:25 -0500

    Complete Part 2 - finished initial styles

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -23,14 +23,18 @@ const Dashboard = ({apiData}) => {
             <div className="dashboard">
                 {apiData.map((item, index) => {
                     return (
-                        <div key={index}>
-                            <img src={item.pic} alt='profile pic'/>
-                            <div>
+                        <div key={index} className='student-container'>
+                            <div className='student-image'>
+                                <img src={item.pic} alt='profile pic'/>
+                            </div>
+                            <div className='student-content'>
                                 <h2>{item.firstName} {item.lastName}</h2>
-                                <p>Email: {item.email}</p>
-                                <p>Company: {item.company}</p>
-                                <p>Skill: {item.skill}</p>
-                                <p>Average: {findAverage(item.grades)}%</p>
+                                <div className='student-info'>
+                                    <p>Email: {item.email}</p>
+                                    <p>Company: {item.company}</p>
+                                    <p>Skill: {item.skill}</p>
+                                    <p>Average: {findAverage(item.grades)}%</p>
+                                </div>
                             </div>
                         </div>
                     )
